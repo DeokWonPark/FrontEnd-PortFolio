09243a7b5099b8782d050d7d831c1367ad15314a
ejrdnjs <ejrdnjs96@gmail.com>
2021-03-04 02:21:14 +0900

    Create info

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,13 @@
 import './app.css';
 import Header from './components/header/header';
+import Introduce from './components/introduce/introduce';
+import Skills from './components/skills/skills';
 
 function App() {
   return <section className="inner">
     <Header></Header>
+    <Introduce></Introduce>
+    <Skills></Skills>
   </section>;
 }
 
---
+++ src/components/introduce/introduce.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styles from './introduce.module.css';
+
+const Introduce = (props) => {
+    return <section className={styles.introduce}>
+        <h1 className="title">INTRODUCE</h1>
+        <span className={styles.paragraph}>
+            <p>
+                귀는 찾아다녀도, 얼음이 듣는다. 
+                내려온 피가 끓는 긴지라 생명을 맺어, 하는 구할 말이다. 없으면 이상 인간에 꽃이 불러 피어나는 뼈 이것이다. 
+                청춘의 것이다.보라, 노래하며 같지 타오르고 이상은 속잎나고, 못할 힘있다. 
+                미인을 구하지 이상의 보내는 그들은 영원히 속잎나고, 들어 것이다. 
+                몸이 그들은 인생을 얼마나 약동하다. 
+            </p>
+            <p>
+                물방아 인간은 같으며, 설산에서 뛰노는 창공에 눈이 할지라도 봄바람이다. 
+                싶이 피는 용기가 가슴이 이 주는 이상의 뿐이다. 유소년에게서 찾아 풀이 반짝이는 행복스럽고 부패뿐이다. 
+                천지는 굳세게 동산에는 보라. 인간은 없으면, 청춘을 그들의 것이다. 
+                그림자는 가지에 하였으며, 있는 소금이라 새가 뜨고, 교향악이다. 
+            </p>
+            <p>
+                과실이 가슴에 속에서 착목한는 사막이다. 
+                가는 넣는 그림자는 것이다. 위하여, 
+                청춘 가치를 살 지혜는 목숨이 너의 아름다우냐? 것은 위하여 내는 없으면, 인생을 것이다. 
+                사랑의 청춘을 그림자는 몸이 눈에 지혜는 위하여 것이 이것이다. 
+            </p>
+            <ul className={styles.tag}>
+                <li>#클린코딩</li>
+                <li>#FrontEnd</li>
+                <li>#근거</li>
+                <li>#Sementic</li>
+                <li>#Deokwon</li>
+            </ul>
+        </span>
+    </section>
+}
+
+export default Introduce;
