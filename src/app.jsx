35dac47c1ce56d902cd086ba279e5a9989a94906
ejrdnjs <ejrdnjs96@gmail.com>
2021-03-03 22:55:46 +0900

    Set up portfolio

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import './app.css';
+import Header from './components/header/header';
+
+function App() {
+  return <section className="inner">
+    <Header></Header>
+  </section>;
+}
+
+export default App;
